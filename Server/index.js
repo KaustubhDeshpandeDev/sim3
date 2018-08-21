cbc62db8c1d990563e4f559db07bb489c4e06c04
Kaustubh Deshpande <kaustubhdeshpande@Kaustubhs-MacBook-Pro.local>
2018-08-21 17:03:17 -0500

    basic setup done

---
+++ Server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const massive = require("massive");
+require("dotenv").config();
+
+const app = express();
+app.use(bodyParser.json());
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    console.log("Database Connected"), app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}.`);
+});
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Auth from "./component/Auth/Auth";
+import Dashboard from "./component/Dashboard/Dashboard";
+import Form from "./component/Form/Form";
+import Nav from "./component/Nav/Nav";
+import Post from "./component/Post/Post";
 
 class App extends Component {
   render() {
@@ -8,10 +13,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Auth />
+          <Dashboard />
+          <Form />
+          <Nav />
+          <Post />
         </p>
       </div>
     );
